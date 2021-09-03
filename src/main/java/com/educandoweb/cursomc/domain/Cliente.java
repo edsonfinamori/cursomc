607924ce63d0f335f829d56a1ecc30df0709be55
Edson Finamori <edsonfi@uol.com.br>
2021-09-03 16:52:25 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/educandoweb/cursomc/domain/Cliente.java
+++ src/main/java/com/educandoweb/cursomc/domain/Cliente.java
@@ -35,6 +35,9 @@ public class Cliente implements Serializable {
 	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos = new ArrayList<>(); 
+	
 	public Cliente() {
 	}
 
@@ -93,6 +96,10 @@ public class Cliente implements Serializable {
 
 	public Set<String> getTelefones() {
 		return telefones;
+	}
+	
+	public List<Pedido> getPedidos() {
+		return pedidos;
 	}
 
 	@Override
