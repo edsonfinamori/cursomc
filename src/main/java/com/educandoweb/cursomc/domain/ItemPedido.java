96968b1a2c2c1d6deac67f27035938fa55108feb
Edson Finamori <edsonfi@uol.com.br>
2021-09-15 15:53:56 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/educandoweb/cursomc/domain/ItemPedido.java
+++ src/main/java/com/educandoweb/cursomc/domain/ItemPedido.java
@@ -6,11 +6,13 @@ import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 
 import com.educandoweb.cursomc.domain.pk.ItemPedidoPK;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class ItemPedido implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
 	
@@ -30,6 +32,7 @@ public class ItemPedido implements Serializable {
 		this.preco = preco;
 	}
 	
+	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
