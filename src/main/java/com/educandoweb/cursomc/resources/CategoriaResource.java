939550c2d42763e5704aaaea845d605b8a363d15
Edson Finamori <edsonfi@uol.com.br>
2021-10-06 16:19:02 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/com/educandoweb/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/educandoweb/cursomc/resources/CategoriaResource.java
@@ -42,7 +42,7 @@ public class CategoriaResource {
 		Categoria obj =  service.fromDTO(objDto);
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-		return ResponseEntity.created(uri).body(obj);
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@PutMapping(value = "/{id}")
