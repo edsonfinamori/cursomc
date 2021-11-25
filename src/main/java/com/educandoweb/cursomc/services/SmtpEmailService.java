ef2562694316aea18bf5f6cf2925eb9c614b7f5f
Edson Finamori <edsonfi@uol.com.br>
2021-11-25 16:15:14 -0300

    Configuracao inicial do Spring Security

--- src/main/java/com/educandoweb/cursomc/services/SmtpEmailService.java
+++ src/main/java/com/educandoweb/cursomc/services/SmtpEmailService.java
@@ -14,7 +14,7 @@ public class SmtpEmailService extends AbstractEmailService {
 	@Autowired
 	private MailSender mailSender;
 	
-	@Autowired
+	@Autowired(required = false)
 	private JavaMailSender javaMailSender;
 	
 	private static final Logger LOG = LoggerFactory.getLogger(SmtpEmailService.class);
