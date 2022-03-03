510f29579ff23aa74309b92900a9105b10f3aa60
Edson Finamori <edsonfi@uol.com.br>
2022-03-03 13:20:36 -0300

    Enviando imagem via endpoint

--- src/main/java/com/educandoweb/cursomc/CursomcApplication.java
+++ src/main/java/com/educandoweb/cursomc/CursomcApplication.java
@@ -1,17 +1,11 @@
 package com.educandoweb.cursomc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.educandoweb.cursomc.services.S3Service;
-
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
-	
-	@Autowired
-	private S3Service s3service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -20,7 +14,5 @@ public class CursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		s3service.uploadFile("C:\\Users\\EDSON\\Downloads\\Gaivotas.jpg");
-
 	}
 }
