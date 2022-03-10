11e38d4d49e2e11aecae8f62d4c1564318a73304
Edson Finamori <edsonfi@uol.com.br>
2022-03-10 18:19:35 -0300

    Salvando a URL da imagem em Cliente

--- src/main/java/com/educandoweb/cursomc/domain/Cliente.java
+++ src/main/java/com/educandoweb/cursomc/domain/Cliente.java
@@ -53,6 +53,8 @@ public class Cliente implements Serializable {
 	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
+	
+	private String imageUrl;
 
 	public Cliente() {
 		addPerfil(Perfil.CLIENTE);
@@ -160,5 +162,13 @@ public class Cliente implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
 	}
 }
